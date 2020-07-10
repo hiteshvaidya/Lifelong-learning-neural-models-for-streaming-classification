b26f2f04b8704809fb607d99509ef9d5cc5a13d2
Hitesh Vaidya <hiteshv.tech@gmail.com>
2020-07-10 15:57:46 -0400

    updated test_autoencoder.py
    code for left network

--- test_autoencoder.py
+++ test_autoencoder.py
@@ -22,8 +22,17 @@ import matplotlib.pyplot as plt
 tf.keras.backend.clear_session()
 
 class Network():
+    '''
+    Class for left network i.e. autoencoder
+    '''
 
-    def __init__(self, n_x, n_z1, n_z2, n_y):
+    def __init__(self, n_x, n_z1, n_z2):
+        '''
+        Constructor for the class
+        :param n_x: no. of input nodes
+        :param n_z1: no. of nodes in layer 1
+        :param n_z2: no. of nodes in layer 2
+        '''
         self.params = []
         self.m1 = tf.Variable(
             tf.random.normal([n_x, n_z1], mean=0.0, stddev=0.1,
@@ -59,7 +68,12 @@ class Network():
         self.params.append(self.m10)
         self.params.append(self.n10)
 
-    def forward(self, x, tau=0.0):
+    def forward(self, x):
+        '''
+        Forward pass of the network
+        :param x: input images
+        :return: reconstructed nodes, left network loss
+        '''
         Z1 = tf.matmul(x, self.m1) + self.n1
         Z1 = tf.nn.sigmoid(Z1)
         Z2 = tf.matmul(Z1, self.m2) + self.n2
@@ -69,23 +83,15 @@ class Network():
         Z0_hat = tf.matmul(Z1, self.m10) + self.n10
         Z0_hat = tf.nn.sigmoid(Z0_hat)
 
-        at_loss = custom_bce(Z1, Z1_hat) + custom_bce(x, Z0_hat)
+        at_loss = custom_MSE(Z1, Z1_hat) + custom_MSE(x, Z0_hat)
         return Z0_hat, at_loss
 
-    def loss(self, x, Z1, Z0, choice='log'):
-        '''
-        y_pred - Tensor of shape (batch_size, size_output)
-        y_true - Tensor of shape (batch_size, size_output)
-        '''
-        y_true_tf = tf.cast(tf.reshape(Z1, (-1, 1)), dtype=tf.float32)
-        y_pred_tf = tf.cast(tf.reshape(Z0, (-1, 1)), dtype=tf.float32)
-        if choice == 'log':
-            return tf.reduce_mean(tf.compat.v1.losses.log_loss(Z0,
-                                                               x)) + tf.reduce_mean(tf.compat.v1.losses.log_loss(Z1, x))
-        elif choice == 'bce':
-            return custom_bce(y_true_tf, y_pred_tf)
-
     def backward(self, x):
+        '''
+        Backward pass
+        :param x: input images
+        :return: None
+        '''
         optimizer = tf.compat.v1.train.GradientDescentOptimizer(
             learning_rate=0.01)
         with tf.GradientTape() as tape:
@@ -93,85 +99,6 @@ class Network():
         grads = tape.gradient(at_loss, self.params)
         optimizer.apply_gradients(zip(grads, self.params),
                                   global_step=tf.compat.v1.train.get_or_create_global_step())
-
-def custom_bce(y, p, offset=1e-7):  # 1e-10
-    '''
-    calculates Binary Cross Entropy value
-    :param y: true labels
-    :param p: predicted output
-    :param offset: threshold for decimal value
-    :return: BCE loss value
-    '''
-    p_ = tf.clip_by_value(p, offset, 1 - offset)
-    vec_bce = -tf.reduce_mean(
-        y * tf.math.log(p_) + (1.0 - y) * tf.math.log(1.0 - p_), axis=1)
-    return tf.reduce_mean(vec_bce)
-
-def custom_KLD(p, q,
-               offset=1e-7):  # q is model predictive/approximating distribution, p is target distribution
-    '''
-    Calculates KL-Divergence
-    :param p: true labels
-    :param q: predicted output
-    :param offset: threshold for decimal value
-    :return: KL-Divergence
-    '''
-    q_ = tf.clip_by_value(q, offset, 1 - offset)
-    p_ = tf.clip_by_value(p, offset, 1 - offset)
-    vec_bce = tf.reduce_sum(p_ * (tf.math.log(p_) - tf.math.log(q_)), axis=-1)
-    return tf.reduce_mean(vec_bce)
-
-def divide_chunks(l, n):
-    '''
-    Divides a list into batches of given size
-    :param l: list
-    :param n: batch size
-    :return: object of subsets of l having sizes n
-    '''
-    # looping till length l
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-
-def batch_loader(data, batch_size):
-    '''
-    load random batches of data
-    :param labels:
-    :param batch_size:
-    :param class_bal:
-    :return:
-    '''
-    indices = np.arange(len(data))
-    for _ in range(5): np.random.shuffle(indices)
-    batches = np.asarray(list(divide_chunks(indices, batch_size)))
-    return batches
-
-def train_metrics():
-    '''
-    Calculates train loss and accuracy
-    :return: None
-    '''
-    loss_at = 0
-    size = 0
-    batches = batch_loader(trainY, batch_size)
-    for batch in batches:
-        Z0_hat, batch_loss = net.forward(trainX[batch])
-        size += batch.shape[0]
-        loss_at += batch_loss * batch.shape[0]
-    train_losses.append(loss_at / size)
-
-def valid_metrics():
-    '''
-    Calculates validation loss and accuracy
-    :return: None
-    '''
-    loss_at = 0
-    size = 0
-    batches = batch_loader(validY, batch_size)
-    for batch in batches:
-        Z0_hat, batch_loss = net.forward(validX[batch])
-        size += batch.shape[0]
-        loss_at += batch_loss * batch.shape[0]
-    valid_losses.append(loss_at / size)
 
 #load data
 trainX = pd.read_csv('mnist_clean/trainX.tsv', sep="\t", header=None,
@@ -186,21 +113,147 @@ validX = pd.read_csv('mnist_clean/validX.tsv', sep="\t", header=None,
                      index_col=False, dtype=np.float32).to_numpy()
 validY = pd.read_csv('mnist_clean/validY.tsv', sep="\t", header=None,
                      index_col=False, dtype=np.float32).to_numpy()
-net = Network(784, 312, 128, 1)
-n_epochs = 10
-batch_size = 32
 
-# declare array for collecting train loss
-# train_losses[t] = [n_epochs x n_tasks]
+# Declare the network
+net = Network(784, 512, 512)
+n_epochs = 4
+batch_size = 50
+
+print('Configuration:')
+print('Layers:', str([784, 512, 512]))
+print('epochs:', 4)
+print('batch_size:', 50)
+
+# Declare losses
 train_losses = []
 test_losses = []
 valid_losses = []
+
+def custom_bce(y, p, offset=1e-7):  # 1e-10
+    '''
+    calculates Binary Cross Entropy value
+    :param y: true labels
+    :param p: predicted output
+    :param offset: threshold for decimal value
+    :return: BCE loss value
+    '''
+    p_ = tf.clip_by_value(p, offset, 1 - offset)
+    vec_bce = -tf.reduce_mean(
+        y * tf.math.log(p_) + (1.0 - y) * tf.math.log(1.0 - p_), axis=1)
+    return tf.reduce_mean(vec_bce)
+
+def custom_KLD(p, q, offset=1e-7):  # q is model predictive/approximating
+    # distribution, p is target distribution
+    '''
+    Calculates KL-Divergence
+    :param p: true labels
+    :param q: predicted output
+    :param offset: threshold for decimal value
+    :return: KL-Divergence
+    '''
+    q_ = tf.clip_by_value(q, offset, 1 - offset)
+    p_ = tf.clip_by_value(p, offset, 1 - offset)
+    vec_bce = tf.reduce_sum(p_ * (tf.math.log(p_) - tf.math.log(q_)), axis=-1)
+    return tf.reduce_mean(vec_bce)
+
+def custom_MSE(y_true, y_pred, offset=1e-7):
+    '''
+    MSE loss function
+    :param y_true: expected correct label
+    :param y_pred: predicted output
+    :param offset: decimal threshold
+    :return: MSE loss value
+    '''
+    y_true = tf.clip_by_value(y_true, offset, 1-offset)
+    y_pred = tf.clip_by_value(y_pred, offset, 1-offset)
+    vec = tf.reduce_sum(tf.math.squared_difference(y_true, y_pred), axis=1)
+    return tf.reduce_mean(vec, axis=0)
+
+def divide_chunks(l, n):
+    '''
+    Divides a list into batches of given size
+    :param l: list
+    :param n: batch size
+    :return: object of subsets of l having sizes n
+    '''
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+def batch_loader(data, batch_size):
+    '''
+    Create batches of indices of data
+    :param data: dataset
+    :param batch_size: batch size
+    :return: batches of data
+    '''
+    indices = np.arange(len(data))
+    for _ in range(5): np.random.shuffle(indices)
+    batches = np.asarray(list(divide_chunks(indices, batch_size)))
+    return batches
+
+def train_metrics(train_record):
+    '''
+    Calculates train loss and accuracy
+    :return: train losses
+    '''
+    loss_at = 0
+    size = 0
+    nums = np.random.choice(trainY.shape[0], int(trainY.shape[0] * 0.4))
+    batches = batch_loader(trainY[nums], 32)
+    for batch in batches:
+        Z0_hat, batch_loss = net.forward(trainX[batch])
+        size += batch.shape[0]
+        loss_at += batch_loss * batch.shape[0]
+    print('\nTrain loss =', loss_at/size)
+    train_record.append(loss_at / size)
+    return train_record
+
+def valid_metrics(valid_record):
+    '''
+    Calculates validation loss and accuracy
+    :return: Validation losses
+    '''
+    loss_at = 0
+    size = 0
+    nums = np.random.choice(validY.shape[0], 1000)
+    batches = batch_loader(validY[nums], 20)
+    for batch in batches:
+        Z0_hat, batch_loss = net.forward(validX[batch])
+        size += batch.shape[0]
+        loss_at += batch_loss * batch.shape[0]
+    print('Validation loss =', loss_at/size)
+    valid_record.append(loss_at / size)
+    return valid_record
+
+def test_metrics(test_record):
+    '''
+    Calculate test loss
+    :param test_record: test losses
+    :return: test losses
+    '''
+    loss_at = 0
+    size = 0
+    batches = batch_loader(testY, 32)
+    for batch in batches:
+        Z0_hat, batch_loss = net.forward(testX[batch])
+        size += batch.shape[0]
+        loss_at += batch_loss * batch.shape[0]
+    print('Test loss =', loss_at / size)
+    test_record.append(loss_at / size)
+    return test_record
+
+# declare array for collecting train loss
+# train_losses[t] = [n_epochs x n_tasks]
+
 # train_accuracy = []
 # valid_accuracy = []
 # test_accuracy = []
 
-train_metrics()
-valid_metrics()
+# calculate initial loss values before training network
+train_losses = train_metrics(train_losses)
+valid_losses = valid_metrics(valid_losses)
+test_losses = test_metrics(test_losses)
 
 # train and calculate BWT, FWT
 tqdm.write('epochs running')
@@ -209,16 +262,11 @@ for epoch in tqdm(range(n_epochs)):
     batches = batch_loader(trainX, batch_size)
     for batch in batches:
         net.backward(trainX[batch])
-    train_metrics()
-    valid_metrics()
 
-# test the model
-test_loss = 0
-batches = batch_loader(testX, batch_size)
-for batch in batches:
-    Z0_hat, batch_loss = net.forward(testX[batch])
-    test_loss += batch_loss * batch.shape[0]
-test_losses.append(test_loss / testX.shape[0])
+    # calculated train, valid, test losses
+    train_losses = train_metrics(train_losses)
+    valid_losses = valid_metrics(valid_losses)
+    test_losses = test_metrics(test_losses)
 
 data_dict = {'train_loss': train_losses,
              'valid_loss': valid_losses,
@@ -238,6 +286,7 @@ def plot_loss():
     '''
     plt.plot(train_losses, label='Train loss')
     plt.plot(valid_losses, label='Validation loss')
+    plt.plot(test_losses, label='Test loss')
     plt.xlabel('epochs')
     plt.ylabel('loss values')
     plt.title('Autoencoder loss')
@@ -310,7 +359,7 @@ def plot_fig():
         mean1 = tf.reduce_mean(results[row][0])
         mean2 = tf.reduce_mean(results[row][1])
         print('Mean of input =', mean1.numpy(), end=',\t')
-        print('Mean of input =', mean2.numpy())
+        print('Mean of output =', mean2.numpy())
         median1 = get_median(tf.sort(results[row][0], axis=-1,
                                      direction='ASCENDING'))
         median2 = get_median(tf.sort(results[row][1], axis=-1,
